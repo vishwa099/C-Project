fe8c94cfd2a21e67514a41f816f3da6e1d606d79
Vishwambhar Prajapati <vishwambharprajapati09@gmail.com>
2023-08-05 17:37:09 +0530

    JustChanged malloc to calloc in DMA.c

--- DMA.c
+++ DMA.c
@@ -5,7 +5,7 @@
 
 int main(){
     int *ptr;
-    ptr=(int *)malloc(5*sizeof(int));         //-----------IN this we forcefully allocated a 20 byte memory for storing our data--------//
+    ptr=(int *)calloc(5,sizeof(int));         //-----------IN this we forcefully allocated a 20 byte memory for storing our data--------//
     ptr[0]=26;
     ptr[1]=15;                                //------if we does not assign some thing to ptr of malloc it does not guarrantee that
     ptr[2]=78;                                         //--it intialises with 0 (it can be a garbage value) but calloc() guarrantees that
